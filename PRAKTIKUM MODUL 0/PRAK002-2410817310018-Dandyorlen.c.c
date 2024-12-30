f691cb27f061b2474a1a1c6356a484a7c7130a61
Dandyor <dandyorlenjunior@gmail.com>
2024-12-30 21:30:32 +0700

    Add files via upload

---
+++ PRAKTIKUM MODUL 0/PRAK002-2410817310018-Dandyorlen.c.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+printf("selamat pagi, dandy orlen jr.p\n");
+printf("selamat siang, dandy orlen jr.p\n");
+printf("selamat malam, dandy orlen jr.p\n");
+return 0;
+}
